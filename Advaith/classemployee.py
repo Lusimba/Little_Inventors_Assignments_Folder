84927fd65438bb190fe7dffa0431ca93dcd16819
Advaithyellai <adithi.advaith@gmail.com>
2019-02-06 08:49:23 +0530

    Added ticket reservation and modified class employee

--- Advaith/classemployee.py
+++ Advaith/classemployee.py
@@ -1,29 +1,31 @@
-class Players:
+class Player:
     def __init__(self, first, last, position):
         self.firstname = first
         self.surname = last
         self.play = position
-    def greeting(self):
+    def greet(self):
         print("Hi I am %s and my last name is %s and I play as %s" %(self.firstname, self.surname, self.play))
-plr_1 = Players('Advaith', 'Yellai', 'Center Defence Mid')
-plr_2 = Players('Wojciech', 'Szczresny', 'Goalkeeper')
-plr_3 = Players('Christiano', 'Ronaldo', 'Right Forward')
-plr_4 = Players('Paula', 'Dybala', 'Left Forward')
-plr_5 = Players('Miralem', 'Pjanic', 'Center Attack mid')
-plr_6 = Players('Douglas', 'Costa', 'Right Wing')
-plr_7 = Players('Emre', 'Can', 'Left wing')
-plr_8 = Players('Medhi','Benatia','Right front defence')
-plr_9 = Players('Leornado','Bonicci','Left front defence')
-plr_10 = Players('Joao','Cancelo','Right back defence')
-plr_11 = Players('Giorgio','Chiellini','Left back defence')
-plr_1.greeting()
-plr_2.greeting()
-plr_3.greeting()
-plr_4.greeting()
-plr_5.greeting()
-plr_6.greeting()
-plr_7.greeting()
-plr_8.greeting()
-plr_9.greeting()
-plr_10.greeting()
-plr_11.greeting()
+
+plr_1 = Player('Advaith   ', 'Yellai   ', 'Center Defence Mid')
+plr_2 = Player('Wojciech  ', 'Szczresny', 'Goalkeeper        ')
+plr_3 = Player('Christiano', 'Ronaldo  ', 'Right Forward     ')
+plr_4 = Player('Paulo     ', 'Dybala   ', 'Left Forward      ')
+plr_5 = Player('Miralem   ', 'Pjanic   ', 'Center Attack mid ')
+plr_6 = Player('Douglas   ', 'Costa    ', 'Right Wing        ')
+plr_7 = Player('Emre      ', 'Can      ', 'Left wing         ')
+plr_8 = Player('Medhi     ', 'Benatia  ','Right front defence')
+plr_9 = Player('Leornado  ', 'Bonucci  ','Left front defence ')
+plr_10 = Player('Joao     ', 'Cancelo  ','Right back defence ')
+plr_11 = Player('Giorgio  ', 'Chiellini','Left back defence  ')
+
+plr_1.greet()
+plr_2.greet()
+plr_3.greet()
+plr_4.greet()
+plr_5.greet()
+plr_6.greet()
+plr_7.greet()
+plr_8.greet()
+plr_9.greet()
+plr_10.greet()
+plr_11.greet()
---
+++ Advaith/reservation.py
@@ -0,0 +1,98 @@
+import time
+
+print("___________________________________")
+lop = []
+loa = []
+count = 15
+size = 0.0
+print("Welcome to Little Inventors travels")
+print("___________________________________")
+
+class reservation:
+    def __init__(self, human, year, arrive, arrivetime, departime, tickets, pay, cost):
+        self.start = 'Hyderabad'
+        self.person = human
+        self.years = year
+        self.arrive = arrive
+        self.arrivation = arrivetime
+        self.departure = departime
+        self.tick = tickets
+        self.payment = pay
+        self.rupees = cost
+    def printer(self):
+        print("Name: %s   From: %s   To: %s   arrival time: %s" %(self.person, self.start, self.arrive, self.arrivation))
+        print("no. of tickets: %s   paying by: %s   cost: %s   departure time: %s   age: %s" %(self.tick, self.payment, self.rupees, self.departure, self.years))
+        print("Have a nice trip")
+        print("******VISIT AGAIN******")
+        print("Thank You")
+
+x = 0
+print("The places are-;")
+print("Delhi, Mumbai, Chennai, Pune, and Bangalore")
+
+arrivation = input("Which place do you want to go to  ")
+arrivation = arrivation.lower()
+people = int(input("How many people are you booking it for  "))
+
+if people > 10:
+    exit("Sorry, you can only book 10 or lesser tickets. Try again")
+
+else:
+    if people >= 5:
+        print("Congratulations! :D You've gotten a bonus ticket for booking 5+ tickets")
+        print("This is for family enjoyment time")
+        people += 1
+
+print("1 = 8 am, 2 = 9 am, 3 = 10 am, 4 = 11 am, 5 = 12 pm, 6 = 1 pm, 7 = 2 pm ... 15 = 10 pm")
+departure = int(input("Pick the number following the time you want to start  "))
+
+for loop in range(0, people):
+    name = input("What's your name  ")
+    age = int(input("How many years are you  "))
+    if age > 15:
+        size += 1.0
+
+    elif age < 15:
+        if age > 5:
+            size += 0.5
+    
+    lop.append(name)
+    loa.append(age)
+    count -= 1
+
+if count <= 0:
+    print("Sorry, we don't have those many tickets")
+
+if arrivation == 'delhi':
+    amount = size * 10000
+    arriving = departure + 10
+
+elif arrivation == 'mumbai':
+    amount = size * 8000
+    arriving = departure + 8
+
+elif arrivation == 'pune':
+    amount = size * 4000
+    arriving = departure + 4
+
+elif arrivation == 'chennai':
+    amount = size * 6000
+    arriving = departure + 6
+
+elif arrivation == 'bangalore':
+    amount = size * 2000
+    arriving = departure + 2
+
+else:
+    exit("There was a problem. Can you please try it again")
+
+print("Smartcard")
+print("Fare")
+print("Money")
+payer = input("How do you want to pay  ")
+
+for loop_2 in range(0, people):
+    printing_1 = reservation(lop[loop_2], loa[loop_2], arrivation, arriving, departure, 15-count, payer, amount)
+    printing_1.printer()
+    print(" ")
+    print(" ")
