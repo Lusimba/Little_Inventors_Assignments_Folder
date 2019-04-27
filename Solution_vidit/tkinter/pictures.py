29119794958a47fb7c99f92bb5bfbcaaad2b975a
Vidit Gupta <gupta.vidit@icloud.com>
2019-04-27 17:37:19 +0530

    kjhgfdsdfghj

---
+++ Solution_vidit/tkinter/pictures.py
@@ -0,0 +1,7 @@
+from tkinter import *
+root = Tk()
+photo = PhotoImage(file = "/Users/ved/Desktop/python/Little_Inventors_Assignments_Folder/Solution_vidit/tkinter2/pictures_for_coding/Shardstone.png")
+pictureLabel = Label(root,image = photo,height = 400,width = 600)
+pictureLabel.pack()
+root.mainloop()
+
---
+++ Solution_vidit/tkinter2/pictures.py
@@ -0,0 +1,6 @@
+from tkinter import *
+root = Tk()
+photo = PhotoImage(file = "Users\ved\Desktop\python\Little_Inventors_Assignments_Folder\Solution_vidit\tkinter\pictures_for_coding\Shardstone.png")
+pictureLabel = Label(root,image = photo,height = 400,width = 600)
+pictureLabel.pack()
+root.mainloop()
