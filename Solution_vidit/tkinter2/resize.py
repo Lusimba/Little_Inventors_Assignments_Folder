901c4616bbc7c7074e1f9bb0c1600026be0cea4f
Vidit Gupta <gupta.vidit@icloud.com>
2019-05-07 22:01:12 +0530

    jiutrdsdfg

--- Solution_vidit/tkinter2/resize.py
+++ Solution_vidit/tkinter2/resize.py
@@ -1,19 +1,27 @@
-from PIL import Image
-file_in = '/Users/ved/Desktop/python/Little_Inventors_Assignments_Folder/Solution_vidit/tkinter2/pictures_for_coding/Shardstone.png'
-pil_image = Image.open(file_in)
-print("image.size   = (%d, %d)" % pil_image.size)
-print("image.format = %s" % pil_image.format)  # 'JPEG'
-print("image.mode   = %s" % pil_image.mode)    # 'RGB'
-file_out = 'Shardstone200x100.png'
-image200x100 = pil_image.resize((200, 100), Image.ANTIALIAS)
-image200x100.save(file_out)
-print("File saved as %s" % file_out)
-import tkinter as tk
-from PIL import ImageTk
-root = tk.Tk()
-root.title(file_in)
-tk_image1 = ImageTk.PhotoImage(pil_image)
-tk_image2 = ImageTk.PhotoImage(image200x100)
-label1 = tk.Label(root,image=tk_image1)
-label1.pack(padx=5, pady=5)
+from tkinter import *
+from PIL import Image, ImageTk
+root = Tk()
+root.title("Title")
+root.geometry("600x600")
+root.configure(background="black")
+
+class Example(Frame):
+    def __init__(self, master, *pargs):
+        Frame.__init__(self, master, *pargs)
+        self.image = Image.open("/Users/ved/Desktop/python/Little_Inventors_Assignments_Folder/Solution_vidit/tkinter2/pictures_for_coding/Shardstone.png")
+        self.img_copy= self.image.copy()
+        self.background_image = ImageTk.PhotoImage(self.image)
+        self.background = Label(self, image=self.background_image)
+        self.background.pack(fill=BOTH, expand=YES)
+        self.background.bind('<Configure>', self._resize_image)
+    def _resize_image(self,event):
+        new_width = event.width
+        new_height = event.height
+        self.image = self.img_copy.resize((new_width, new_height))
+        self.background_image = ImageTk.PhotoImage(self.image)
+        self.background.configure(image =  self.background_image)
+e = Example(root)
+e.pack(fill=BOTH, expand=YES)
+
+
 root.mainloop()
